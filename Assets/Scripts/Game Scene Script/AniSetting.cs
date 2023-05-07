07269642c48cab7485d755ea18bc668b59f0e9b3
bamin0502 <100828741+bamin0502@users.noreply.github.com>
2023-05-07 17:07:34 +0900

    Update AniSetting.cs

--- Assets/Scripts/Game Scene Script/AniSetting.cs
+++ Assets/Scripts/Game Scene Script/AniSetting.cs
@@ -22,6 +22,7 @@ namespace mino
 
     public class AniSetting : MonoBehaviour
     {
+        
         Animator ani;
         // Start is called before the first frame update
         void Start()

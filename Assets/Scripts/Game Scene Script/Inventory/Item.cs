e65b2533d3adfecf50adf9e079e117a6c69c09c3
bamin0502 <100828741+bamin0502@users.noreply.github.com>
2023-05-18 01:47:57 +0900

    일단 인벤토리 시스템 다시 재구축중
    OnDrawGizmos()를 이용해서 캐릭터 앞에 레이케스트(광선을 발사해서) 해당되는 아이템을 발견시 텍스트를 띄우고 그 상태에서 해당되는 키를 입력시 먹게하고 벗어났을때는 해당 텍스트가 사라지도록 설정

--- Assets/Scripts/Game Scene Script/Inventory/Item.cs
+++ Assets/Scripts/Game Scene Script/Inventory/Item.cs
@@ -1,94 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Sirenix.OdinInspector;
 
-public class Item : SerializedMonoBehaviour
+[CreateAssetMenu(fileName = "New Item", menuName = "New Item/item")]
+public class Item : ScriptableObject
 {
-    public enum TYPE { HP, MP }
-
-    public TYPE type;           // 아이템의 타입.
-    public Sprite DefaultImg;   // 기본 이미지.
-    public int MaxCount;        // 겹칠수 있는 최대 숫자.  
-
-    public string Name;         // 아이템의 이름.
-    
-    public void Init(string Name, int MaxCount)
-    {
-        // 이름에 따라 타입을 결정.
-        switch (Name)
-        {
-            case "HP": type = TYPE.HP; break;
-            case "MP": type = TYPE.MP; break;
-        }
-
-        this.Name = Name;           // 이름을 초기화.
-        this.MaxCount = MaxCount;   // 겹칠수 있는 한계 개수를 초기화.
-
-
-        Sprite[] spr = ObjManager.Call().spr;       // 이미지 배열을 가져온다.
-        int Count = ObjManager.Call().spr.Length;   // 이미지 배열의 총 크기.
-                                                    // 이미지의 이름과 아이템의 이름을 비교.
-        for (int i = 0; i < Count; i++)
-        {
-            // 두 이름이 같으면 그 이미지를 DefaultImg에 셋팅.
-            if (spr[i].name == this.Name)
-            {
-                DefaultImg = spr[i];
-                break;
-            }
-        }
+    public enum ItemType 
+    { 
+        Used,
+        Throw
     }
+    public string itemName;
+    public ItemType itemType;
+    public Sprite itemImage;
+    public GameObject itemPrefab;
 
-    // 인벤토리에 접근하기 위한 변수.
-    public Inventory Iv;
-
-    void Awake()
-    {
-        // 태그명이 "Inventory"인 객체의 GameObject를 반환한다.
-        // 반환된 객체가 가지고 있는 스크립트를 GetComponent를 통해 가져온다.
-        GameObject inventoryObject = GameObject.FindGameObjectWithTag("Inventory");
-        if (inventoryObject == null)
-        {
-            Debug.LogError("Inventory 스크립트를 가진 게임 오브젝트를 찾을 수 없습니다.");
-            return;
-        }
-
-        // 인벤토리 오브젝트가 존재하면 해당 오브젝트의 Inventory 컴포넌트를 가져옵니다.
-        Iv = inventoryObject.GetComponent<Inventory>();
-        if (Iv == null)
-        {
-            Debug.LogError("Inventory 컴포넌트를 찾을 수 없습니다.");
-            return;
-        }
-    }
-
-    public bool AddItem(Inventory inventory)
-    {
-        return inventory.AddItem(this);
-    }
-    // 충돌체크
-    void OnTriggerEnter(Collider other)
-    {
-        // 충돌한 객체가 플레이어인 경우에만 아이템을 추가
-        if (other.CompareTag("Player"))
-        {
-            Inventory Iv = FindObjectOfType<Inventory>();
-            if (Iv != null)
-            {
-                if (AddItem(Iv))
-                {
-                    gameObject.SetActive(false); // 아이템을 비활성화
-                }
-                else
-                {
-                    Debug.Log("아이템이 가득 찼습니다.");
-                }
-            }
-            else
-            {
-                Debug.LogError("Inventory 컴포넌트를 찾을 수 없습니다.");
-            }
-        }
-    }
+    public string weaponType;
 }
---
+++ Assets/Scripts/Game Scene Script/Inventory/ItemDropController.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class ItemDropController : MonoBehaviour
+{
+    [SerializeField]
+    private float range;  // 아이템 습득이 가능한 최대 거리
+
+    private bool pickupActivated = false;  // 아이템 습득 가능할시 True 
+
+    private RaycastHit hitInfo;  // 충돌체 정보 저장
+
+    [SerializeField]
+    private LayerMask layerMask;  // 특정 레이어를 가진 오브젝트에 대해서만 습득할 수 있어야 한다.
+
+    [SerializeField]
+    private TMP_Text actionText;  // 행동을 보여 줄 텍스트
+
+    void FixedUpdate()
+    {
+        CheckItem();
+        TryAction();
+        OnDrawGizmos();
+    }
+
+    private void TryAction()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            CheckItem();
+            CanPickUp();
+        }
+    }
+
+    private void CheckItem()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out hitInfo, range, layerMask))
+        {
+            if (hitInfo.transform.tag == "Item")
+            {
+                ItemInfoAppear();
+            }
+        }
+        else
+            ItemInfoDisappear();
+    }
+
+    private void ItemInfoAppear()
+    {
+        pickupActivated = true;
+        actionText.gameObject.SetActive(true);
+        actionText.text = hitInfo.transform.GetComponent<ItemPickup>().item.itemName + " 획득 " + "<color=yellow>" + "(E)" + "</color>";
+    }
+
+    private void ItemInfoDisappear()
+    {
+        pickupActivated = false;
+        actionText.gameObject.SetActive(false);
+    }
+
+    private void CanPickUp()
+    {
+        if (pickupActivated)
+        {
+            if (hitInfo.transform != null)
+            {
+                Debug.Log(hitInfo.transform.GetComponent<ItemPickup>().item.itemName + " 획득 했습니다.");  // 인벤토리 넣기
+                Destroy(hitInfo.transform.gameObject);
+                ItemInfoDisappear();
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+    #if UNITY_EDITOR
+        // 현재 설정된 layerMask 값을 시각적으로 표시합니다.
+        Handles.Label(transform.position, "Layer Mask: " + LayerMask.LayerToName(layerMask));
+
+        // 정면으로 레이를 그려서 레이어가 앞으로 나가는지 여부를 확인합니다.
+        Debug.DrawRay(transform.position, transform.forward * range, Color.red);
+    #endif
+    }
+}

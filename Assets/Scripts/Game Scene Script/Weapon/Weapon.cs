8fe10a6b67eece7b91ea8c6c3ad9c7ea3c86c9ec
escv12 <62186356+escv12@users.noreply.github.com>
2023-05-18 14:20:20 +0900

    플레이어 공격 애니메이션
    무기 종류별 플레이어 공격 애니메이션 설정

--- Assets/Scripts/Game Scene Script/Weapon/Weapon.cs
+++ Assets/Scripts/Game Scene Script/Weapon/Weapon.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum WeaponType{
+    Gun, // 3
+    Bow, //4 
+    OneHanded, //5
+    TwoHanded, //6
+}
+
+
 public abstract class Weapon : MonoBehaviour
 {
     //임성훈 
     public string weaponName; //무기명
+    public float range;
     public int damage; //데미지
     public bool isEquipped = false; //장착 여부
     public float attackInterval;
+
+    public WeaponType weaponType;
 
 
     public abstract void Attack(Transform target);

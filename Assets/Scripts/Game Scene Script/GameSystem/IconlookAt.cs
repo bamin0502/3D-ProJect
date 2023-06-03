29e45d8f5a024f6d39017bd7ac376429aef99d43
bamin0502 <100828741+bamin0502@users.noreply.github.com>
2023-06-03 12:43:55 +0900

    플레이어가 몬스터보다 위로 가거나 몬스터가 감지해서 쫒아오면 아이콘이 뒤집히는 현상 수정

--- Assets/Scripts/Game Scene Script/GameSystem/IconlookAt.cs
+++ Assets/Scripts/Game Scene Script/GameSystem/IconlookAt.cs
@@ -8,15 +8,13 @@ public class IconlookAt : MonoBehaviour
 
     private void Awake()
     {
-        // 프리팹이 인스턴스화될 때 미니맵용 카메라를 찾아 할당합니다.
         _cam = GameObject.FindGameObjectWithTag("MiniMap Camera").GetComponent<Camera>();
-        // "MiniMapCamera"는 미니맵용 카메라에 할당된 태그입니다. 필요에 따라 태그를 변경해야합니다.
     }
     // Update is called once per frame
     void Update()
     {
         Vector3 lookAtPosition = _cam.transform.position;
         lookAtPosition.x = transform.position.x;
-        transform.LookAt(lookAtPosition);
+        transform.LookAt(lookAtPosition,_cam.transform.up);
     }
 }

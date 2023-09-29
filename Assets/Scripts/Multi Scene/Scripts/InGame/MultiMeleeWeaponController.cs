a52fe3e8f93728cac73fa8125efd6cb9bcee5c53
escv12 <62186356+escv12@users.noreply.github.com>
2023-09-29 16:07:58 +0900

    추가
    무기 공격 모션 동기화
    이동 위치 핀 생성

---
+++ Assets/Scripts/Multi Scene/Scripts/InGame/MultiMeleeWeaponController.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using mino;
+using UnityEngine;
+
+public class MultiMeleeWeaponController : MonoBehaviour
+{
+    private MultiWeaponController _weaponController;
+    private MultiPlayerMovement _playerMovement;
+
+    private bool _isAttack;
+    private RaycastHit hitInfo;
+    private Transform currentTarget;
+    private Weapon currentWeapon;
+    private Coroutine attackRoutine;
+    private static readonly int BowAttack = Animator.StringToHash("BowAttack");
+    private static readonly int OneHandedAttack = Animator.StringToHash("OneHandedAttack");
+    private static readonly int TwoHandedAttack = Animator.StringToHash("TwoHandedAttack");
+    private void Start()
+    {
+        _weaponController = GetComponent<MultiWeaponController>();
+        _playerMovement = GetComponent<MultiPlayerMovement>();
+    }
+    void Update()
+    {
+        TryAttack();
+    }
+    private void TryAttack()
+    {
+        currentWeapon = _weaponController.equippedWeapon;
+        currentTarget = _weaponController.currentTarget;
+
+        if (currentWeapon == null) return;
+
+        if (!_weaponController.isAttack && attackRoutine != null)
+        {
+            _isAttack = false;
+            StopCoroutine(attackRoutine);
+        }
+
+        if (_weaponController.isAttack && !_isAttack)
+        {
+            attackRoutine = StartCoroutine(AttackCoroutine());
+        }
+    }
+    private IEnumerator AttackCoroutine()
+    {
+        _isAttack = true;
+
+        _playerMovement.ChangedState(PlayerState.HammerAttackIdle);
+        MultiScene.Instance.BroadCastingAnimation((int)PlayerState.HammerAttackIdle);
+
+        var weaponInterval = currentWeapon.attackInterval;
+        var weaponType = currentWeapon.weaponType;
+
+        while (_isAttack)
+        {
+            switch (weaponType)
+            {
+                case WeaponType.Bow:
+                    _playerMovement.SetAnimationTrigger(BowAttack);
+                    MultiScene.Instance.BroadCastingAnimation(BowAttack, true);
+                    StartCoroutine(ArrowSpawnCoroutine());
+                    break;
+                case WeaponType.OneHanded:
+                    _playerMovement.SetAnimationTrigger(OneHandedAttack);
+                    MultiScene.Instance.BroadCastingAnimation(OneHandedAttack, true);
+                    break;
+                case WeaponType.TwoHanded:
+                    _playerMovement.SetAnimationTrigger(TwoHandedAttack);
+                    MultiScene.Instance.BroadCastingAnimation(TwoHandedAttack, true);
+                    break;
+                default:
+                    break;
+            }
+
+            yield return new WaitForSeconds(weaponInterval);
+        }
+    }
+
+    private IEnumerator ArrowSpawnCoroutine(){
+        yield return new WaitForSeconds(0.1f);
+        SoundManager.instance.PlaySE("Bow_String");
+        var currentBow = currentWeapon.GetComponent<RangedWeapon>();
+        var arrow = Instantiate(currentBow.projectilePrefab, currentBow.arrowPos.position, Quaternion.LookRotation(currentBow.arrowPos.forward));
+        arrow.transform.parent = currentBow.transform;
+        StartCoroutine(arrow.GetComponent<Projectile>().ShotCoroutine(currentTarget));
+    }
+}

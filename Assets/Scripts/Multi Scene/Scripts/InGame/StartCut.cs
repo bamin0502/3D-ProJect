e47463adf0a5fb4646eb982b72426373aeb8bea8
bamin0502 <100828741+bamin0502@users.noreply.github.com>
2023-10-12 11:03:44 +0900

    ESC 컷신 스킵 기능추가

--- Assets/Scripts/Multi Scene/Scripts/InGame/StartCut.cs
+++ Assets/Scripts/Multi Scene/Scripts/InGame/StartCut.cs
@@ -18,10 +18,20 @@ public class StartCut : MonoBehaviour
         {
             // 컷신이 시작되면 BGM을 중지
             SoundManager.instance.bgmAudioSource.Stop();
+
         }
 
         // 컷신 종료 이벤트 핸들러 등록
         _playableDirector.stopped += OnCutsceneEnd;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // 'Esc' 키를 누르면 컷신을 넘깁니다.
+            _playableDirector.time = _playableDirector.duration;
+        }  
     }
     // 컷신 종료 시 호출될 이벤트 핸들러
     public void OnCutsceneEnd(PlayableDirector director)

71e75d6be3df35188670b58ecd59e9b028d6de71
bamin0502 <100828741+bamin0502@users.noreply.github.com>
2023-10-19 13:53:02 +0900

    일단 아이템 드랍관련 에러 출력되는 거 수정 & 몬스터 관련 수정 필
    빌드본에서 몬스터 인덱스 값이 -1로 출력되서 ArgumentOutOfRangeException이 출력 되고 있음 테스트할때 에디터1에 빌드본1로 동시에 실행시켜서 확인해봐야함
    
    Co-Authored-By: escv12 <62186356+escv12@users.noreply.github.com>

--- Assets/Scripts/Multi Scene/Scripts/InGame/Multi_Inventory/MultiItemDropController.cs
+++ Assets/Scripts/Multi Scene/Scripts/InGame/Multi_Inventory/MultiItemDropController.cs
@@ -37,8 +37,7 @@ public class MultiItemDropController : MonoBehaviour
 
     private void CheckItem()
     {
-        // 아이템 콜라이더 배열에 충돌된 콜라이더를 넣어줍니다.
-        itemColliders = new Collider[10];  // 적절한 크기로 조정하세요.
+        itemColliders = new Collider[10];  // 테스트 하면서 적절한 크기로 조정하세요.
 
         // 메모리 할당 없이 아이템을 감지합니다.
         int itemCount = Physics.OverlapSphereNonAlloc(transform.position, pickupRadius, itemColliders, layerMask);
@@ -47,7 +46,7 @@ public class MultiItemDropController : MonoBehaviour
         {
             for (int i = 0; i < itemCount; i++)
             {
-                if (itemColliders[i].CompareTag("Item"))
+                if (itemColliders[i] != null && itemColliders[i].CompareTag("Item"))
                 {
                     ItemInfoAppear();
                     return; // 가장 가까운 아이템만 처리하도록 리턴
@@ -56,7 +55,6 @@ public class MultiItemDropController : MonoBehaviour
         }
         else
         {
-            itemColliders = null;
             ItemInfoDisappear();
         }
     }
@@ -64,15 +62,22 @@ public class MultiItemDropController : MonoBehaviour
     private void ItemInfoAppear()
     {
         pickupActivated = true;
-        actionText.gameObject.SetActive(true);
-    
-        string itemName = itemColliders[0].GetComponent<ItemPickup>().item.itemName;
-        actionText.text = itemName+" 아이템 획득 " + "<color=yellow>" + "(G)" + "</color>";
+        //actionText.gameObject.SetActive(true);
+
+        if (itemColliders[0] != null)
+        {
+            string itemName = itemColliders[0].GetComponent<ItemPickup>().item.itemName;
+            actionText.text = itemName + " 아이템 획득 " + "<color=yellow>" + "(G)" + "</color>";
+        }
     }
     private void ItemInfoDisappear()
     {
         pickupActivated = false;
-        actionText.gameObject.SetActive(false);
+        if (actionText != null)
+        {
+            actionText.text = "";
+            //actionText.gameObject.SetActive(false);
+        }
     }
 
     private void PickUpItem()
